0953cd87e99c841957b4e3641d794ce12588283f
xxozs <ebashynasfe@yandex.ru>
2021-11-25 20:25:41 +0300

    added video section and design mix

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,13 @@
 $(function(){
+  
+  var mixer = mixitup('.design__content');
 
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 1500
+    autoplaySpeed: 3000
   });
 
 });

974f4253c3d7bcbcaf743913704789fafa50bc4a
xxozs <ebashynasfe@yandex.ru>
2021-11-29 14:52:15 +0300

    added rules and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function(){
   
-  var mixer = mixitup('.design__content');
+  // var mixer = mixitup('.design__content');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
   $('.top-slider__inner').slick({
     dots: true,
